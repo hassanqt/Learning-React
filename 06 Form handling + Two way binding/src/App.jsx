1b9b64239aca0d69cc24311e51c7550b99883193
Hassan <hassannqt@gmail.com>
2026-09-02 10:58:43 +0500

    add two way binding

--- 06 Form handling + Two way binding/src/App.jsx
+++ 06 Form handling + Two way binding/src/App.jsx
@@ -1,29 +1,34 @@
 import React from "react";
+import { useState } from "react";
 
 const App = () => {
+  const [val, setVal] = useState("");
+
   return (
     <div className="w-full h-screen flex justify-center flex-col items-center">
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          console.log("form submitted");
+          console.log("form submitted by", val);
+          setVal("")
         }}
         className="flex justify-center items-center flex-col gap-4"
       >
         <input
           type="text"
           name=""
+          onChange={(e) => {
+            setVal(e.target.value);
+          }}
+          value={val}
           id="user"
           placeholder="Enter Username..."
-          className="px-2 bg-gray-300 py-1 border rounded-lg"
-          onChange={(e) => {
-            console.log(e.target.value)
-          }}
+          className="select-none px-2 bg-gray-300 py-1 border rounded-lg"
         />
         <input
           type="submit"
           value="Submit"
-          className="px-3 py-1 border rounded-lg bg-[#ddd]"
+          className="px-3 py-1 cursor-pointer border rounded-lg bg-[#ddd]"
         />
       </form>
     </div>
