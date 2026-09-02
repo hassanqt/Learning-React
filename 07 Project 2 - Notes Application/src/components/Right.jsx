75bcd7e8829974ea30e0bc5d31f16f1c4ab8b0a5
Hassan <hassannqt@gmail.com>
2026-09-02 13:59:01 +0500

    Update Right.jsx

--- 07 Project 2 - Notes Application/src/components/Right.jsx
+++ 07 Project 2 - Notes Application/src/components/Right.jsx
@@ -1,5 +1,5 @@
 const Right = () => {
-  return <div className="basis-[50%] border-l border-white p-8 flex flex-col gap-10">
+  return <div className="basis-[50%] border-l-2 border-white p-8 flex flex-col gap-10">
     <h2 className="text-2xl font-semibold text-white">Recent Notes:</h2>
     <div className="flex flex-col justify-start gap-6 flex-wrap">
       <div className="w-full h-20 bg-black/60 text-white flex items-center px-8 cursor-pointer rounded-xl">Web Dev Note</div>
