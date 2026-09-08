ee5bbc0b029732a1116cacd3d612af4b507a7712
Hassan <hassannqt@gmail.com>
2026-09-09 00:58:37 +0500

    add - api response handling

--- 08 Local Storage/src/App.jsx
+++ 08 Local Storage/src/App.jsx
@@ -1,7 +1,29 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import axios from "axios";
+import { useState } from "react";
 
-export default App
+const App = () => {
+  const [dataa, setDataa] = useState([]);
+
+  let fetchData = async () => {
+    let data = await axios.get("https://dog.ceo/api/breeds/image/random");
+    setDataa(data.data);
+    console.log(data.data);
+    
+  };
+
+  return (
+    <div className="w-full h-screen flex flex-col gap-10 justify-center items-center">
+      <button
+        onClick={fetchData}
+        className="px-6 py-2 bg-rose-500 text-white cursor-pointer active:translate-y-px rounded-full"
+      >
+        Click me to fetch random dog image
+      </button>
+      <div>
+        <img className="size-auto" src={dataa.message} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- 08 Local Storage/vite.config.js
+++ 08 Local Storage/vite.config.js
@@ -1,9 +1,14 @@
-import react from '@vitejs/plugin-react'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
 })
